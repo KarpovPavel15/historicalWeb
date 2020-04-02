f85604bf1b10ca9f417a1ea8da00d8e9dca6e037
KPVBY <kpvbygm@gmail.com>
2020-04-02 18:12:19 +0300

    spec

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const MainPage = () => {
     return (
         <div>
-            <header>HEADER1</header>
+            <header>HEADER</header>
             <main>main</main>
             <footer>footer</footer>
         </div>
