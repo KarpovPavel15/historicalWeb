f8ef09a4be343e8b1ecff8e53444cada757fbcbc
KPVBY <kpvbygm@gmail.com>
2020-04-06 16:27:18 +0300

    refactoring header structure

--- src/modules/header/component/header.jsx
+++ src/modules/header/component/header.jsx
@@ -5,11 +5,13 @@ import React from 'react';
 export const Header = () => {
     return (
         <header className="header">
-            <div className="header__area">
+            <div className="header__navigation">
                 <div><a>Logo</a></div>
                 <div><a>nav</a></div>
                 <div><a>userOptions</a></div>
             </div>
+            <div className="header__image"/>
         </header>
+
     )
 };
