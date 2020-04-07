1739d2403a1e95119cdc82ed450ea9b2af4f7b38
KPVBY <kpvbygm@gmail.com>
2020-04-07 15:18:50 +0300

    refactoring

--- src/shared/components/logo/logo.jsx
+++ src/shared/components/logo/logo.jsx
@@ -1,11 +1,12 @@
 import './logo.scss'
+import logo from 'assets/Logo.png'
 
 import React from 'react';
 
 export const Logo = () => {
     return (
         <div className="logo">
-
+            <img src={logo}/>
         </div>
     )
 };
