f1a742f681a76326fe82e5ce4c4718df0fc3983a
Nikita <nikita.rusilko98@gmail.com>
2020-04-20 01:45:51 +0300

    Fixed warning in persons

--- src/shared/components/persons/persons.jsx
+++ src/shared/components/persons/persons.jsx
@@ -1,30 +1,32 @@
-import './persons.scss'
-import React from 'react';
-import PropTypes from 'prop-types';
+import "./persons.scss";
+import React from "react";
+import PropTypes from "prop-types";
 
-import { Person } from 'shared/components';
+import { Person } from "shared/components";
 
-export const Persons = ({personsList}) => (
-        <div className="persons">
-            {
-                personsList.map(element => element.id % 2 !== 0
-                    ? <div className="persons__left-person"><Person person={element} key={element.id}/></div>
-                    : <div className="persons__right-person"><Person person={element} key={element.id}/></div>
-                )
-            }
+export const Persons = ({ personsList }) => {
+  return (
+    <div className="persons">
+      {personsList.map((element) => (
+        <div className={`persons__${element.id % 2 !== 0 ? `left` : `right`}-person`} key={element.id}>
+          <Person person={element} />
         </div>
-);
+      ))}
+    </div>
+  );
+};
 
 Persons.propTypes = {
-    personsList: PropTypes.arrayOf(
-        PropTypes.shape({
-            img: PropTypes.string,
-            name: PropTypes.string,
-            scope: PropTypes.string
-        })).isRequired,
-    personsForSlider: PropTypes.bool
+  personsList: PropTypes.arrayOf(
+    PropTypes.shape({
+      img: PropTypes.string,
+      name: PropTypes.string,
+      scope: PropTypes.string,
+    })
+  ).isRequired,
+  personsForSlider: PropTypes.bool,
 };
 
 Persons.defaultProps = {
-    personsForSlider: false
+  personsForSlider: false,
 };
