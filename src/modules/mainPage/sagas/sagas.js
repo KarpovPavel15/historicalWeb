7dd949f3a4ff892ca9e9725752b80f9502d9d67a
KPVBY <kpvbygm@gmail.com>
2020-05-01 00:16:59 +0300

    refactoring

--- src/modules/mainPage/sagas/sagas.js
+++ src/modules/mainPage/sagas/sagas.js
@@ -1,5 +1,5 @@
 import { call, put, takeLatest } from 'redux-saga/effects';
-import { API } from 'api/rest';
+import { API } from 'api/rest/API';
 import { setMainPageContentSuccessAction } from 'modules/mainPage';
 
 function* setMainPageContent() {
