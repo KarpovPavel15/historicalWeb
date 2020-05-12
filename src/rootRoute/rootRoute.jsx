6d17f67d283efec2edfda8cb9a7330515e561b8a
KPVBY <kpvbygm@gmail.com>
2020-05-12 17:56:50 +0300

    add react scroll functional for header

--- src/rootRoute/rootRoute.jsx
+++ src/rootRoute/rootRoute.jsx
@@ -6,9 +6,5 @@ import { default as MainPage } from 'modules/mainPage';
 export const RootRoute = () => (
     <Switch>
         <Route exact path='' component={MainPage}/>
-        <Route path="/route1" render={() => <div>Route</div>}/>
-        <Route path="/route2" render={() => <div>Persones</div>}/>
-        <Route path="/route3" render={() => <div>Team</div>}/>
-        <Route path="/route4" render={() => <div>Souvenirs</div>}/>
     </Switch>
 );
