3dfdda6bbf379acbefe14c27db7b802b0a9b220b
KPVBY <kpvbygm@gmail.com>
2020-06-05 01:40:35 +0300

    no server problem

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
+const multer  = require('multer');
+const upload = multer({ dest: './dist/'});
 
 
 const PORT = process.env.PORT || 8080;
 
+app.get('/routes/:id', upload.single('dist'),(req, res)=> res.status(204).end());
 
 app.use(express.static('dist'));
 
