a2df143710f8387d9ab5f19efb7ce1ba175b89ba
KPVBY <kpvbygm@gmail.com>
2020-07-26 14:59:42 +0300

    added sagas testing

--- src/assets/room/index.js
+++
@@ -1,1 +0,0 @@
-export { default as RoomImage } from './roomImage.png'
--- src/assets/routes/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Route1 } from './Route1.png';
-export { default as Route2 }  from './Route2.png';
-export { default as Route3 }  from './Route3.png';
-export { default as Route4 }  from './Route4.png';
